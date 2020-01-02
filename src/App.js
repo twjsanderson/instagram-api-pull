532f9c20c760a9bb46df8faa342f2f49d73af16a
Tom Sanderson <tom.w.j.sanderson@gmail.com>
2020-01-02 13:41:15 -0500

    moved jsx inside render()

--- src/App.js
+++ src/App.js
@@ -13,27 +13,28 @@ class App extends React.Component {
     .then(results => {
       return results.json();
     }).then(data => {
-      let pictures = data.graphql.user.edge_owner_to_timeline_media.edges.map(result => {
-        let resultDetail = result.node;
-        return (
-          <div key={resultDetail.id}>
-            <img
-              src={resultDetail.thumbnail_src} 
-              alt={resultDetail.accessibility_caption}
-            />
-          </div>
-        )
-      })
+      let pictures = data.graphql.user.edge_owner_to_timeline_media.edges;
       this.setState({ pictures: pictures });
-    })
+    }).catch(error => {
+      console.log(error)
+    });
   }
 
   render() {
     return (
       <div>
         <h1>Instagram Test</h1>
-        
-          { this.state.pictures }
+        { 
+          this.state.pictures.map(picture => {
+            let pictureDetail = picture.node;
+            return <div key={pictureDetail.id}>
+              <img
+                src={pictureDetail.thumbnail_src} 
+                alt={pictureDetail.accessibility_caption}
+              />
+            </div>
+          })
+        }
       </div>
     );
   }
